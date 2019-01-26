71a67cd8252030291aef86b1256f517bf9731687
woodcat <pmknkaek@gmail.com>
2019-01-25 17:55:44 -0700

    fixing bugs

--- captcha.php
+++ captcha.php
@@ -1,30 +1,30 @@
 <?php
 //This class requires a session to be created on the page and does not create its own
 
-session_start();
-
-$captcha = new captcha;
+session_start(); //temporary session for testing purposes
+$captcha = new captcha(); //temporary constructor for testing purposes
 
 class captcha
 {
     function __construct()
     {
-        echo "am i dead?";
-        if(!isset($_SESSION[$randomID])){
-            $captcha->generateID();
-            captcha::generateCaptcha();
-        } 
-        else
+        echo 'dead for sure';
+        if(!isset($_SESSION['randomID'])) //checks to see if the captcha has already been created
         {
-            $_SESSION[$mousePosition] = ['x' => $_GET["x"], 'y' => $_GET["y"]];
-            checkSuccess();
+            $_SESSION['randomID']=$this->generateID();
+            $this->generateCaptcha();
+        } 
+        else //if session has been created, it polls for the mouse position and checks to see if the success condition has been met yet
+        {
+            $_SESSION['mousePosition'] = ['x' => $_GET["x"], 'y' => $_GET["y"]];
+            //checkSuccess();
         }
         
     }
 
     private function generateID()
     {
-        $generatedID = 
+        $generatedID = //instantiate the random data for the captcha
         [
             'challenge' => null, // Possible options: smallest, biggest, striped
             'generated_at' => date('m/d/Y h:i:s', time()),
@@ -38,6 +38,7 @@ class captcha
             ]
         ];
 
+        //this is some pretty stupid randomization that will be replaced with something more sensible
         switch (rand(0,99)%3)
         {
             case 0:
@@ -51,8 +52,6 @@ class captcha
                 break;
         }
         Header('Content-type: text');
-        echo $generatedID;
-        
         return $generatedID;
     }
 
@@ -60,21 +59,21 @@ class captcha
     {
         $layers = 
         [
-            'bg' => imagecreatefrompng('./bglayer'),
+            'bg' => imagecreatefrompng('./bglayer.png'),
             'left' => imagecreatetruecolor(420, 240),
             'right' => imagecreatetruecolor(420, 240)
         ];
         $fishes = 
         [
-            'normal' => imagecreatefrompng('./normal'),
-            'striped' => imagecreatefrompng('./stripe')
+            'normal' => imagecreatefrompng('./normal.png'),
+            'striped' => imagecreatefrompng('./stripe.png')
         ];
-        $font = imageloadfont('./dpcommic.ttf');
-        $textColor = imagecolorallocate($bg, 160, 15, 150);
+        $font = imageloadfont('./dpcomic.ttf');
+        $textColor = imagecolorallocate($layers['bg'], 160, 15, 150);
         $fish_w = 96;
         $fish_h = 54;
 
-        imagestring($bg, $font, 25, 225, "Catch the " . $_SESSION[$randomID['challenge']] . " fish!", $textColor);
+        imagestring($layers['bg'], $font, 25, 225, "Catch the " . $_SESSION['randomID']['challenge'] . " fish!", $textColor);
 
         for($i=1; $i <= 2; $i++) 
         {
@@ -82,25 +81,25 @@ class captcha
             {
                 foreach($fishes as $fish)
                 {
-                    imageflip($fish, $IMG_FLIP_HORIZONTAL);
+                    imageflip($fish, IMG_FLIP_HORIZONTAL);
                 }
             } 
-            foreach($_SESSION[$randomID[$fish]] as $fishy)
+            foreach($_SESSION['randomID']['fish'] as $fishy)
             {
-                switch(fishy['trait'])
+                switch($fishy['trait'])
                 {
                     case 'none':
-                        imagecopy($layers[$i], $normal, fishy['x'], fishy['y'], 0, 0, $fish_w, $fish_h);
+                        imagecopy($layers[$i], $normal, $fishy['x'], $fishy['y'], 0, 0, $fish_w, $fish_h);
                         break;
                     case 'striped':
-                        imagecopy($layers[$i], $striped, fishy['x'], fishy['y'], 0, 0, $fish_w, $fish_h);
+                        imagecopy($layers[$i], $striped, $fishy['x'], $fishy['y'], 0, 0, $fish_w, $fish_h);
                         break;
                     case 'big':
-                        imagecopyresized($layers[$i], $normal, fishy['x'], fishy['y'], 0, 0, floor($fish_h*1.5), floor($fish_h*1.5),$fish_w, $fish_h);
+                        imagecopyresized($layers[$i], $normal, $fishy['x'], $fishy['y'], 0, 0, floor($fish_h*1.5), floor($fish_h*1.5),$fish_w, $fish_h);
                         break;
                     case 'dead':
-                        $deadfish = imageflip($normal, $IMG_FLIP_VERTICAL);
-                        imagecopy($layers[$i], $deadfish, fishy['x'], fishy['y'], 0, 0, $fish_w, $fish_h);
+                        $deadfish = imageflip($normal, IMG_FLIP_VERTICAL);
+                        imagecopy($layers[$i], $deadfish, $fishy['x'], $fishy['y'], 0, 0, $fish_w, $fish_h);
                         break;
                 }
             }
@@ -118,5 +117,5 @@ class captcha
 
     }
 }
-    
+
 ?>
