8bed61a431e96bb64bd72db65d9fd8dd6e3a1f69
woodcat <pmknkaek@gmail.com>
2019-02-26 18:05:11 -0700

    changed update to function on delta time instead of getAnimationFrame()

--- captcha.js
+++ captcha.js
@@ -1,6 +1,6 @@
 function captcha()
 {
-    var maxFrameRate = 25; 
+    var pixelsPerSec = 50.0; //$pixelsPerSec must equal this number in captcha.php
 
     var background = new Image();
     var left = 
@@ -25,6 +25,7 @@ function captcha()
 
     this.canvasElement = 
     {
+        lastUpdate : Date.now(),
         initialize : function()
         {
             canvas = document.createElement('canvas');
@@ -32,30 +33,27 @@ function captcha()
             canvas.height = 240;
             context = canvas.getContext("2d");
             document.body.prepend(canvas);
-            this.update();
+            var tick = setInterval(this.update, 40);
         },
         update : function()
         {
-            //TODO: will have to redo the movement so that the layers are moved with respect to time and not number of frames
-            //      so the positions can be calculated by the server without needing to pass it the number of frames drawn
-            setTimeout(function()
-            {
-                left.x -= 2; //$pixelsPerFrame must equal these numbers in captcha.php
-                right.x += 2;
+            if(isNaN(this.lastUpdate)){this.lastUpdate = Date.now();}
+            var now = Date.now();
+            var dt = now - this.lastUpdate;
+            this.lastUpdate = now;
+            left.x -= 50.0*dt/1000; 
+            right.x += 50.0*dt/1000;
 
-                if(left.x<=-420){left.x = -1;}
-                if(right.x>=420){right.x = 1;}
+            if(left.x<=-420){left.x = -1;}
+            if(right.x>=420){right.x = 1;}
 
-                context.drawImage(background, 0, 0);
+            context.drawImage(background, 0, 0);
 
-                context.drawImage(left.layer, -left.x, 0, 420+left.x, 240, 0, 0, 420+left.x, 240);
-                context.drawImage(left.layer, 0, 0, -left.x, 240, 420+left.x, 0, -left.x, 240);
+            context.drawImage(left.layer, -left.x, 0, 420+left.x, 240, 0, 0, 420+left.x, 240);
+            context.drawImage(left.layer, 0, 0, -left.x, 240, 420+left.x, 0, -left.x, 240);
 
-                context.drawImage(right.layer, 420-right.x, 0, right.x, 240, 0, 0, right.x, 240);
-                context.drawImage(right.layer, 0, 0, 420-right.x, 240, right.x, 0, 420-right.x, 240);
-
-                requestAnimationFrame(canvasElement.update);
-            }, 1000/maxFrameRate);
+            context.drawImage(right.layer, 420-right.x, 0, right.x, 240, 0, 0, right.x, 240);
+            context.drawImage(right.layer, 0, 0, 420-right.x, 240, right.x, 0, 420-right.x, 240);
         }
     }
 
