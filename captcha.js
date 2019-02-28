24b71ba1f9e35fae041d4cd74e81611db2442457
woodcat <pmknkaek@gmail.com>
2019-02-28 16:45:45 -0700

    add lotsa comments for clarity

--- captcha.js
+++ captcha.js
@@ -6,7 +6,7 @@ function captcha()
     var left = 
     {
         layer : new Image(),
-        x : 0 //x position of bottom-leftmost pixel of the orignal layer on the canvas
+        x : 0 //x pos of bottom-leftmost pixel of layer
     };
     var right = 
     {
@@ -27,26 +27,33 @@ function captcha()
     {
         initialize : function()
         {
-            canvas = document.createElement('canvas');
+            var captchaDiv = document.getElementById("captcha"); //find <div> block to insert canvas
+            canvas = document.createElement("canvas"); //initialize canvas
             canvas.width = 420;
             canvas.height = 240;
-            context = canvas.getContext("2d");
-            document.getElementById("captcha").append(canvas);
-            this.lastUpdate = Date.now();
-            var tick = setInterval(canvasElement.update, 40);
+            context = canvas.getContext("2d"); //initialize context to draw to
+            captchaDiv.append(canvas); //insert canvas into page
+
+            //captchaDiv.onmousemove("updateCoords(event)"); //tracks mouse position
+            //captchaDiv.onmousedown("useNet(event)"); //handles when a user starts to drag net
+            //captchaDiv.onmouseup("dropNet(event)"); //handles when user stops dragging net
+
+            this.lastUpdate = Date.now(); //initialize data for animation loop
+            var tick = setInterval(canvasElement.update, 40); //start animation loop
         },
         update : function()
         {
-            var now = Date.now();
+            var now = Date.now(); //handle delta time
             var dt = now - canvasElement.lastUpdate;
             canvasElement.lastUpdate = now;
-            left.x -= 50.0*dt/1000; 
+
+            left.x -= 50.0*dt/1000; //update positions of layers
             right.x += 50.0*dt/1000;
 
             if(left.x<=-420){left.x = -1;}
             if(right.x>=420){right.x = 1;}
 
-            context.drawImage(background, 0, 0);
+            context.drawImage(background, 0, 0); //draw all layers
 
             context.drawImage(left.layer, -left.x, 0, 420+left.x, 240, 0, 0, 420+left.x, 240);
             context.drawImage(left.layer, 0, 0, -left.x, 240, 420+left.x, 0, -left.x, 240);
@@ -56,20 +63,20 @@ function captcha()
         }
     }
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/captcha.php", true);
-    xhr.send();
-    xhr.onreadystatechange = function()
+    var challengeXHR = new XMLHttpRequest(); //initial request to server to generate and begin challenge
+    challengeXHR.open('GET', "/captcha.php", true); //will call captcha.php with no parameters
+    challengeXHR.send(); //sends request
+    challengeXHR.onreadystatechange = function() //listening for response
     {
-        if (xhr.readyState == 4 && xhr.status == 200)
+        if (challengeXHR.readyState == 4 && challengeXHR.status == 200) //if response is valid
         {
-            var response = JSON.parse(xhr.responseText);
+            var response = JSON.parse(challengeXHR.responseText); //parse response as a json
             
-            background.src = response.background;
+            background.src = response.background; //extract image data from parsed json
             left.layer.src = response.left;
             right.layer.src = response.right;
         }
     }
 
-    canvasElement.initialize();
+    canvasElement.initialize(); //build the challenge
 }
