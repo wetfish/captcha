ca23558492ce84e9dbc4b26bb71f624cbc19f9e8
Quercus Felis <oakenfeline@gmail.com>
2019-06-11 22:07:45 -0600

    add fail state & more comments

--- captcha.js
+++ captcha.js
@@ -1,45 +1,48 @@
-var user = 
+var user = //information for drawing the net
 {
-    inFrame : false,
+    inFrame : false, //is cursor inside canvas
     x : 0,
     y : 0,
     usingNet : false //is user dragging net
 }
 
-function updateCoords(event)
+function updateCoords(event) //UI trigger for updating net location
 {
     if(event.type == 'mousemove') //checks if interaction is with a mouse cursor or touch event
     {
-        user.x = event.offsetX;
+        user.x = event.offsetX; //mouse movement event
         user.y = event.offsetY;
     }
-    else
+    else //handles touch events for mobile
     {
-        user.x = event.touches[0].clientX;
+        user.x = event.touches[0].clientX; //grabs coords for 1st touch point
         user.y = event.touches[0].clientY;
     }
 }
 
-function useNet(event)
+function useNet(event) //UI trigger for clicking while mouse in canvas
 {
     user.usingNet = true;
     user.inFrame = true;
 }
 
-function dropNet(event, inframe)
+function dropNet(event, inframe) //UI trigger for not clicking and leaving canvas
 {
     user.usingNet = false;
     user.inFrame = inframe;
 }
 
-function captcha()
+function captcha() //the actual captcha 'class' 
 {
     var pixelsPerSec = 50.0; //$pixelsPerSec must equal this number in captcha.php
-    var success = false;
+    var success = false; //if true, triggers success state on next update
+    var failTimeout = 15000; //timeout in milliseconds
+    var fail = false; //if true, triggers fail state on next update
 
-    var background = new Image();
+    var background = new Image(); //initialize image data
     var welcome = new Image();
     var successImg = new Image();
+    var failImg = new Image();
     var left = 
     {
         layer : new Image(),
@@ -56,17 +59,18 @@ function captcha()
         drag: new Image()
     }
 
-    var intervals = 
+    var intervals = //timing events for challenge
     {
-        tick: null,
-        check: null
+        tick: null, //updates/animates canvas
+        check: null, //pings server for success state
+        timeout: null //fails attempt after a time
     }
 
-    var canvas;
-    var context;
+    var canvas; //canvas DOM element
+    var context; //canvas drawing context
     var captchaDiv = document.getElementById("captcha"); //find <div> block to insert and remove canvas
 
-    this.canvasElement = 
+    this.canvasElement = //events that control canvas
     {
         initialize : function()
         {
@@ -126,13 +130,30 @@ function captcha()
 
             if(success)
             {
-                context.drawImage(successImg, canvas.width/2-successImg.width/2, canvas.height/2-successImg.height/2);
-                clearInterval(intervals.check);
-                clearInterval(intervals.tick);
+                context.drawImage(successImg, canvas.width/2-successImg.width/2, canvas.height/2-successImg.height/2); //display checkmark
+                clearInterval(intervals.check); //stops pinging for success
+                clearInterval(intervals.tick); //stops updating canvas
+                clearTimeout(intervals.timeout); //clear fail state
+            }
+
+            if(fail)
+            {
+                context.drawImage(failImg, 0, 0); //display failure message
                 
+                clearInterval(intervals.check); //stops pinging for success
+                clearInterval(intervals.tick); //stops updating canvas
+                clearTimeout(intervals.timeout); //reset fail state
+                fail = false;
+
+                retrieveChallenge(); //generate new challenge
+                left.x = 0;
+                right.x = 0;
+
+                document.getElementById("canvas").addEventListener("click", canvasElement.start);
+                document.getElementById("canvas").addEventListener("touchend", canvasElement.start); 
             }
         },
-        start : function()
+        start : function() //runs when canvas is clicked for the first time
         {
             document.getElementById("canvas").removeEventListener("click", canvasElement.start);
             document.getElementById("canvas").removeEventListener("touchstart", canvasElement.start);
@@ -140,76 +161,77 @@ function captcha()
             intervals.tick = setInterval(canvasElement.update, 34); //start animation loop
             checkSuccess(true); //tells php to start
             intervals.check = setInterval(checkSuccess, 100); //check if captcha has been completed
+            intervals.timeout = setTimeout(function(){fail = true;}, failTimeout); //sets fail timeout
         }
     }
 
-    initialize(); 
+    initialize(); //called on page load, might move this to a more conspicuous spot later
 
-    async function initialize()
+    async function initialize() //initializes all image variables above and initializes the canvas
     {
-        //retrieve generated image data
-        var retrieveChallenge = new Promise(function()
-        {
-            var challengeXHR = new XMLHttpRequest(); //initial request to server to generate and begin challenge
-            challengeXHR.withCredentials = true;
-            challengeXHR.open('GET', "/captcha.php?new='true'", true); //will call captcha.php with no parameters
-            challengeXHR.send(); //sends request
-            challengeXHR.onreadystatechange = function() //listening for response
-            {
-                if (challengeXHR.readyState == 4 && challengeXHR.status == 200) //if response is valid
-                {
-                    var response = JSON.parse(challengeXHR.responseText); //parse response as a json
-            
-                    background.src = response.background; //extract image data from parsed json
-                    left.layer.src = response.left;
-                    right.layer.src = response.right;
-                    challengeXHR.abort;
-                }
-            }
-            
-        });
-
         //retrieve known image data
-        Promise.all([
+        Promise.all([ //retrieve static assets
             retrieveAsset("welcome.png"),
             retrieveAsset("success.png"),
             retrieveAsset("net1.png"),
-            retrieveAsset("net2.png")
+            retrieveAsset("net2.png"),
+            retrieveAsset("fail.png")
         ])
-        .then(function(blobs)
+        .then(function(blobs) //assign static assets
         {
             welcome.src = blobs[0];
             successImg.src = blobs[1];
             net.loose.src = blobs[2];
             net.drag.src = blobs[3];
+            failImg.src = blobs[4];
         })
         .then(function()
         {
-            retrieveChallenge; //TODO: figure out why adding this to a promise chain freezes the chain, despite appearing to complete successfully
-            canvasElement.initialize(); //buildda the challenge
-            welcome.onload = function(){ context.drawImage(welcome, 0, 0); };
+            retrieveChallenge(); //TODO: figure out why adding this to a promise chain freezes the chain, despite appearing to complete successfully
+            canvasElement.initialize(); //build the challenge
+            welcome.onload = function(){ context.drawImage(welcome, 0, 0); }; //draws the welcome image
         })
         .catch(function(error){ console.log(error); });
     }
 
-    function retrieveAsset(assetName)
+    function retrieveAsset(assetName) //retrieve static assets
     {
-        var request = new Request('captcha-assets/'+assetName);
+        var request = new Request('captcha-assets/'+assetName); //initialize request
         
-        return fetch(request)
+        return fetch(request) //fetches, then later returns blob url for asset
         .then(function(response)
         {
-            return response.blob();
+            return response.blob(); //extracts blob
         })
         .then(function(blob)
         {
-            return URL.createObjectURL(blob);
+            return URL.createObjectURL(blob); //generates url
         });
     }
 
-    function checkSuccess(first) 
+    function retrieveChallenge() //retrieve generated image data
     {
-        if(user.usingNet || first){
+        var challengeXHR = new XMLHttpRequest(); //initial request to server to generate and begin challenge
+        challengeXHR.withCredentials = true;
+        challengeXHR.open('GET', "/captcha.php?new='true'", true); //will call captcha.php with no parameters
+        challengeXHR.send(); //sends request
+        challengeXHR.onreadystatechange = function() //listening for response
+        {
+            if (challengeXHR.readyState == 4 && challengeXHR.status == 200) //if response is valid
+            {
+                var response = JSON.parse(challengeXHR.responseText); //parse response as a json
+        
+                background.src = response.background; //extract image data from parsed json
+                left.layer.src = response.left;
+                right.layer.src = response.right;
+                challengeXHR.abort;
+            }
+        }   
+    }
+
+    function checkSuccess(first) //ping server for success state
+    {
+        if(user.usingNet || first){ //only calls if user is actively attempting solution
             var successXHR = new XMLHttpRequest(); //request to server to check for a success
             successXHR.withCredentials = true;
             successXHR.open('GET', "/captcha.php?x="+user.x+"&y="+user.y, true); //will call captcha.php with user data
@@ -218,7 +240,7 @@ function captcha()
             {
                 if (successXHR.readyState == 4 && successXHR.status == 200) //if response is valid
                 {
-                    success = successXHR.responseText == "true";
+                    success = successXHR.responseText == "true"; //checks for success response from server
                 }
             }
         }
